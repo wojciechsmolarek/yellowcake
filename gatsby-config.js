aa5e0e06a10fff67eae2955068c0fe20a8244090
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2020-11-01 16:33:28 +0100

    Edit SiteURL

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Yellowcake - Wojciech Smolarek',
+    siteUrl: 'https://github.com/wojciechsmolarek/yellowcake'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
